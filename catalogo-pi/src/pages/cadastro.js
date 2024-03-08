6c1a0d8426b535c3a5ec6c755d2615873c9247ec
RodolfoSantana8 <129907500+RodolfoSantana8@users.noreply.github.com>
2024-03-07 22:21:36 -0300

    Atualização para transformar virgula em ponto

--- catalogo-pi/src/pages/cadastro.js
+++ catalogo-pi/src/pages/cadastro.js
@@ -3,61 +3,95 @@ import CustomCheckboxLabel from "@/components/CustomCheckboxLabel";
 import CustomInputLabel from "@/components/CustomInputLabel";
 import CustomTextAreaLabel from "@/components/CustomTextAreaLabel";
 import axios from "axios";
+//import { sources } from "next/dist/compiled/webpack/webpack";
 import { useState } from "react";
 
-export default function cadastro(){
+export default function cadastro() {
+  const [dadosProduto, setDadosProduto] = useState({
+    id: 0,
+    nome: "",
+    descricao: "",
+    preco: 0,
+    novidade: false,
+    disponivel: false,
+  });
+  
+  function converte() {
+    dadosProduto.preco = dadosProduto.preco.replace(",", ".");
+  }
+  
+  function handleSalvar() {
+    converte();
+    //Enviaremos o post para a API
+    axios
+      .post("https://localhost:7087/api/Produto", dadosProduto)
+      .then((res) => console.log(res));
+  }
 
-    const [dadosProduto, setDadosProduto] = useState({
-                                                        id : 0,
-                                                        nome : "",
-                                                        descricao: "",
-                                                        preco: 0,
-                                                        novidade: false,
-                                                        disponivel: false
-                                                    })
+  function handleChange(e) {
+    console.log(e.target.id, e.target.value);
 
-    function handleSalvar()
-    {
-        //Enviaremos o post para a API
-        axios.post("https://localhost:7087/api/Produto", dadosProduto)
-            .then(res => console.log(res))
+    if (e.target.type == "checkbox") {
+      dadosProduto[e.target.id] = e.target.checked;
+    } else {
+      dadosProduto[e.target.id] = e.target.value;
     }
 
-    function handleChange(e){
-        console.log(e.target.id, e.target.value)
+    setDadosProduto(dadosProduto);
 
+    console.log(dadosProduto);
+  }
 
-        if (e.target.type == "checkbox")
-        {
-            dadosProduto[e.target.id] = e.target.checked
-        }else{
-            dadosProduto[e.target.id] = e.target.value
-        }
-
-        setDadosProduto(dadosProduto)
-
-        console.log(dadosProduto)
-    }
-
-    return (
-        <>
-        <Cabecalho />
-        <div className="container-fluid">
-            <div className="row">
-                <CustomInputLabel id="nome" onChange={handleChange} texto="Nome" col="8"/>
-                <CustomInputLabel id="preco" onChange={handleChange} texto="Preço" col="4"/>
-                <CustomTextAreaLabel id="descricao" onChange={handleChange} texto="Descrição" col="12"/>
-                <CustomInputLabel id="imagem" onChange={handleChange} texto="URL da Imagem" col="12" />
-                <CustomCheckboxLabel id="disponivel" onChange={handleChange} texto="Disponivel" col="6" />
-                <CustomCheckboxLabel id="novidade" onChange={handleChange} texto="Novidade" col="6" />
-
-            </div>
-            <div className="row">
-                <div className="col-12">
-                    <button className="btn btn-primary" onClick={handleSalvar}>Salvar</button>
-                </div>
-            </div>
+  return (
+    <>
+      <Cabecalho />
+      <div className="container-fluid">
+        <div className="row">
+          <CustomInputLabel
+            id="nome"
+            onChange={handleChange}
+            texto="Nome"
+            col="8"
+          />
+          <CustomInputLabel
+            id="preco"
+            onChange={handleChange}
+            texto="Preço"
+            col="4"
+          />
+          <CustomTextAreaLabel
+            id="descricao"
+            onChange={handleChange}
+            texto="Descrição"
+            col="12"
+          />
+          <CustomInputLabel
+            id="imagem"
+            onChange={handleChange}
+            texto="URL da Imagem"
+            col="12"
+          />
+          <CustomCheckboxLabel
+            id="disponivel"
+            onChange={handleChange}
+            texto="Disponivel"
+            col="6"
+          />
+          <CustomCheckboxLabel
+            id="novidade"
+            onChange={handleChange}
+            texto="Novidade"
+            col="6"
+          />
         </div>
-        </>
-    )
+        <div className="row">
+          <div className="col-12">
+            <button className="btn btn-primary" onClick={handleSalvar}>
+              Salvar
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
